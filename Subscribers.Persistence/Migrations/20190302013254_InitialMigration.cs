e3176570186c0e5714a5b5fcc29156179fb18785
Vittoria Zago <vittoriazago1@hotmail.com>
2019-03-01 22:37:24 -0300

    Configurating database and migrations

---
+++ Subscribers.Persistence/Migrations/20190302013254_InitialMigration.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Subscribers.Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ClientId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    BirthDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ClientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    ServiceId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    RegisterDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.ServiceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subscribers",
+                columns: table => new
+                {
+                    SubscriberId = table.Column<Guid>(nullable: false),
+                    ClientId = table.Column<Guid>(nullable: false),
+                    ServiceId = table.Column<Guid>(nullable: false),
+                    SubscribeDate = table.Column<DateTime>(nullable: false),
+                    UnsubscribeDate = table.Column<DateTime>(nullable: true),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscribers", x => x.SubscriberId);
+                    table.ForeignKey(
+                        name: "FK_Subscribers_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "ClientId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Subscribers_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "ServiceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscribers_ClientId",
+                table: "Subscribers",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscribers_ServiceId",
+                table: "Subscribers",
+                column: "ServiceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscribers");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+        }
+    }
+}
---
+++ Subscribers.Persistence/SubscribersDbContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Subscribers.Persistence
+{
+    public class SubscribersDbContextFactory : IDesignTimeDbContextFactory<SubscribersDbContext>
+    {
+        private const string ConnectionStringName = "SubscribersDatabase";
+        private const string AspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
+
+        public SubscribersDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<SubscribersDbContext>();
+            var environment = Environment.GetEnvironmentVariable(AspNetCoreEnvironment);
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory() 
+                    + string.Format("{0}..{0}Subscribers.WebAPI", Path.DirectorySeparatorChar))
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.Local.json", optional: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString(ConnectionStringName);
+            builder.UseSqlServer(connectionString);
+
+            return new SubscribersDbContext(builder.Options);
+        }
+    }
+}
