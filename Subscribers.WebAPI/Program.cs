285b264f86d9c1306c6665618dc16dafb38167c9
Vittoria Zago <vittoriazago1@hotmail.com>
2019-03-07 23:16:19 -0300

    Calling seed process on webapi start

--- Subscribers.WebAPI/Program.cs
+++ Subscribers.WebAPI/Program.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Subscribers.WebAPI
@@ -14,7 +16,25 @@ namespace Subscribers.WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetService<Persistence.SubscribersDbContext>();
+                    context.Database.Migrate();
+
+                    Persistence.SubscribersSeed.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
