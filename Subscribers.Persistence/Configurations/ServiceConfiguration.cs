1453597cc1eb71e15544af726a88266cb36e4ad4
Vittoria Zago <vittoriazago1@hotmail.com>
2019-03-13 19:31:21 -0300

    Fixing namespace references

--- Subscribers.Persistence/Configurations/ServiceConfiguration.cs
+++ Subscribers.Persistence/Configurations/ServiceConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Subscribers.Domain;
+using Subscribers.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
