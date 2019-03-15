fbf309125826779ca0c95c0a8d3babbe5292fa29
Vittoria Zago <vittoriazago1@hotmail.com>
2019-03-14 21:12:42 -0300

    Configuranting swagger

--- Subscribers.WebAPI/Startup.cs
+++ Subscribers.WebAPI/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -11,7 +12,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
 using Subscribers.Persistence;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Subscribers.WebAPI
 {
@@ -33,6 +36,31 @@ namespace Subscribers.WebAPI
             services.AddDbContext<SubscribersDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("SubscribersDatabase")));
 
+            // Configurando o serviço de documentação do Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Subscribers Service",
+                        Version = "v1",
+                        Description = "Service representing ASPNET CORE",
+                        Contact = new Contact
+                        {
+                            Name = "Vittoria Zago",
+                            Url = "https://github.com/vittoriazago/"
+                        }
+                    });
+
+                string caminhoAplicacao =
+                    PlatformServices.Default.Application.ApplicationBasePath;
+                string nomeAplicacao =
+                    PlatformServices.Default.Application.ApplicationName;
+                string caminhoXmlDoc =
+                    Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
+
+                c.IncludeXmlComments(caminhoXmlDoc);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +77,14 @@ namespace Subscribers.WebAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Ativando middlewares para uso do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "Subscribers Service");
+            });
         }
     }
 }
